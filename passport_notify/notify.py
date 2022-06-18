3fabce7b6a4ec57dcf5ab996d645d02fea31c722
singurty <singurty@gmail.com>
2022-06-18 22:02:35 +0545

    ready for new

--- passport_notify/notify.py
+++ passport_notify/notify.py
@@ -8,14 +8,16 @@ import time
 telegram_send.send(messages=["-------------BOT STARTED {} -------------".format(datetime.datetime.now())])
 
 # get first request
-prevRequest = requests.get("https://emrtds.nepalpassport.gov.np/iups-api/calendars/11/false")
+prevRequest = requests.get("https://emrtds.nepalpassport.gov.np/iups-api/calendars/79/false")
 telegram_send.send(messages=["Initial response:", prevRequest.text])
+print("Initial response:", prevRequest.text)
 
 while True:
     # confirmation sake
     print(prevRequest.text)
     time.sleep(300)
-    newRequest = requests.get("https://emrtds.nepalpassport.gov.np/iups-api/calendars/11/false")
+    newRequest = requests.get("https://emrtds.nepalpassport.gov.np/iups-api/calendars/79/false")
     if prevRequest.text != newRequest.text:
         telegram_send.send(messages=["CHANGE IN REQUEST", "New reqeust:", newRequest.text])
+        print("CHANGE IN REQUEST", "New reqeust:", newRequest.text)
         prevRequest = newRequest
